c05f4c45e4b105e1100828c270a4ddb848e8ac4f
Maria Blair <maria.klachko@gmail.com>
2016-08-15 12:49:18 -0700

    Edited index.html title. Edited app.js: added Player.

--- js/app.js
+++ js/app.js
@@ -24,6 +24,17 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(dt) {
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 
 // Now instantiate your objects.
