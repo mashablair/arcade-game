74649d15640db89229f8b52dc8527e9ae6430b2b
Maria Blair <maria.klachko@gmail.com>
2016-08-15 23:41:27 -0700

    Added coordinates, Math.random() method to speed, movement for the enemy prototype, if statement for collision , render for both enemy and player.  Player now displays.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+var Enemy = function(x,y) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    //x and y coordinates:
+    this.x = x;
+    this.y = y;
+
+    //speed of the bugs with Math.random() from
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    this.speed = Math.floor((Math.random()*200)+100);
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +19,19 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x <= 600){
+        this.x = this.x + this.speed * dt;
+    } else {
+        this.x = -2;
+    }
+
+    //collision: if player comes within 25px of enemy's x and y coordinates,
+    //game will reset:
+    if(player.x >= this.x - 25 && player.x <= this.x + 25) {
+        if(player.y >= this.y - 25 && player.y <= this.y + 25) {
+            this.reset();
+        }
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,11 +39,13 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// Player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
     this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400;
 };
 
 Player.prototype.update = function(dt) {
@@ -36,10 +56,16 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//Reset player to beginning position
+Player.prototype.reset = function() {
+  this.x = 200;
+  this.y = 400;
+};
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Place the Player object in a variable called player
 var allEnemies = [];
 
 
@@ -48,7 +74,7 @@ var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
