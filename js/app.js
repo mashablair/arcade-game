318a97ca42e87116541beddb7479a8aefc08815f
Maria Blair <maria.klachko@gmail.com>
2016-08-15 18:31:46 -0700

    Fixed all errors in Console by: creating allEnemies array and placing the player object in a var called player. Now I have a canvass displaying.

--- js/app.js
+++ js/app.js
@@ -40,6 +40,10 @@ Player.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+
+
+var player = new Player();
 
 
 
